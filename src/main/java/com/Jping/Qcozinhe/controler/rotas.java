4d149149f1bb09d30546e14f9a695f038f62f7f3
MICRO <134sandrinha@gmail.com>
2022-01-02 14:20:04 -0300

    ok

--- src/main/java/com/Jping/Qcozinhe/controler/rotas.java
+++ src/main/java/com/Jping/Qcozinhe/controler/rotas.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @RestController
 public class rotas {
 
@@ -35,9 +40,22 @@ public class rotas {
     }
 
     @GetMapping(path = "consulta/nome/{nome}")
-    public ResponseEntity consulta_by_nome(@PathVariable("nome") String nome){
-        return repositorio.findById(nome).map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
+    public Object consulta_by_nome(@PathVariable("nome") String nome){
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conexao = DriverManager.getConnection("jdbc:postgres://ybkhmkmtoetxme:271e89c0ea20652b1b73735b78b9b06b73698742b21fa06e5fcbecb8df6d19e7@ec2-18-210-153-185.compute-1.amazonaws.com:5432/dfipshh492396p","ybkhmkmtoetxme","271e89c0ea20652b1b73735b78b9b06b73698742b21fa06e5fcbecb8df6d19e7");
+            ResultSet rsCliente = conexao.createStatement().executeQuery("select * from Receitas_db");
+            String resposta ="";
+            while(rsCliente.next()){
+                 resposta = resposta + rsCliente.getString("nome_receitas")+"   " + rsCliente.getString("ingredientes")+"  "+rsCliente.getString("modo_preparo")+" "+rsCliente.getString("nome");
+            }
+            return resposta;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
     }
 
     @GetMapping(path ="salvar/{nome_receita}/{ingredientes}/{modo_preparo}/{nome}")
