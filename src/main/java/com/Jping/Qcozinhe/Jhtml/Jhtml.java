e16680903c5c77a8477dac2a8ee9c0eb1eb3b109
MICRO <134sandrinha@gmail.com>
2022-01-07 18:31:25 -0300

    ok

--- src/main/java/com/Jping/Qcozinhe/Jhtml/Jhtml.java
+++ src/main/java/com/Jping/Qcozinhe/Jhtml/Jhtml.java
@@ -3,6 +3,10 @@ package com.Jping.Qcozinhe.Jhtml;
 import com.Jping.Qcozinhe.Refatorando.ReplaceString;
 import com.Jping.Qcozinhe.request.Requisiçao;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Jhtml {
     public String html(){
         Requisiçao requisiçao = new Requisiçao();
@@ -10,6 +14,7 @@ public class Jhtml {
 
         String text = requisiçao.request();
         String Html = replaceString.replace(text);
+
         String nomeImages = replaceString.nomeImage(text);
         return "<!DOCTYPE html>\n" +
                 "<html lang='pt-br'>\n" +
@@ -21,4 +26,20 @@ public class Jhtml {
                 "<body>\n" +Html+ " <br><br>"+nomeImages+"</body>\n" +
                 "</html>";
     }
+    public static String replaceNomeImg(String tagHtml,String nome){
+
+        String[] nome_img = nome.split(",");
+        Path caminho = Paths.get(nome_img[0]);
+        String html = null;
+        try {
+            byte[] texto = Files.readAllBytes(caminho);
+            String leitura = new String(texto);
+            tagHtml.replace("#=","<img src='data:image/jpeg;base64,"+leitura+"'>");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
+
--- src/main/java/com/Jping/Qcozinhe/Refatorando/ReplaceString.java
+++ src/main/java/com/Jping/Qcozinhe/Refatorando/ReplaceString.java
@@ -12,7 +12,7 @@ public class ReplaceString {
         String text4 = text3.replace("ingredientes", "<h2>INGREDIENTES</h2>");
         String text5 = text4.replace("modo_preparo", "<h2>MODO DE PREPARO</h2>");
         String text6 = text5.replace("nome", "<h2>NOME</h2>");
-        String text7 = text6.replace('"', '"');
+        String text7 = text6.replace('"', ' ');
         String text8 = text7.replace("_", " ");
         String text9 = text8.replace("[", "");
         String text10 = text9.replace("]", "");
