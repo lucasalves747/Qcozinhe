07a50da59355e6c0752d3b1aa7ecb72ecfb298fe
MICRO <134sandrinha@gmail.com>
2022-01-07 17:32:59 -0300

    ok

--- src/main/java/com/Jping/Qcozinhe/Refatorando/ReplaceString.java
+++ src/main/java/com/Jping/Qcozinhe/Refatorando/ReplaceString.java
@@ -3,21 +3,23 @@ package com.Jping.Qcozinhe.Refatorando;
 public class ReplaceString {
 
     public  String replace(String frase) {
+
         String text = frase.replace("{", "");
         String text1 = text.replace("}", "");
-        String text2 = text1.replace(":", "");
+        String texta2 = text1.replace("nome img", "#=");
+        String text2 = texta2.replace(":", "");
         String text3 = text2.replace("nome_receitas", "#=  <br><br><br>-----------------------------------<br><br><h2>NOME DA RECEITA</h2>");
         String text4 = text3.replace("ingredientes", "<h2>INGREDIENTES</h2>");
         String text5 = text4.replace("modo_preparo", "<h2>MODO DE PREPARO</h2>");
-        //String text6 = text5.replace("nome", "<h2>NOME</h2>");
-        String text7 = text5.replace('"', '"');
+        String text6 = text5.replace("nome", "<h2>NOME</h2>");
+        String text7 = text6.replace('"', '"');
         String text8 = text7.replace("_", " ");
         String text9 = text8.replace("[", "");
         String text10 = text9.replace("]", "");
-        String text11 = text10.replace("nome img", "#=");
 
 
-        return text11;
+
+        return text10;
     }
     public String nomeImage(String text){
         String texto = replace(text);
